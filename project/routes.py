0e0a5e3b842d3e088861127cde348ed21c5294a4
Adeel Barki <adeelbarki@gmail.com>
2019-01-11 20:28:59 +0500

    Add delete modal

--- project/routes.py
+++ project/routes.py
@@ -28,7 +28,8 @@ def new_item():
 @app.route("/item/<item_id>/")
 def item(item_id):
     item = Item.query.get_or_404(item_id)
-    return render_template('item.html', title=item.title, item=item)
+    category = Category.query.filter_by(id=item.cat_id).first()
+    return render_template('item.html', title=item.title, item=item, category=category)
 
 
 @app.route("/item/<item_id>/edit", methods=['GET', 'POST'])
@@ -50,3 +51,12 @@ def edit_item(item_id):
         form.select.data = category.name
     return render_template('addNewItem.html', title='Edit Item', form=form, 
                                 legend='Edit Item')
+
+
+@app.route("/item/<item_id>/delete", methods=['POST'])
+def delete_item(item_id):
+    item = Item.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    flash('Item is deleted', 'success')
+    return redirect(url_for('catalog'))
